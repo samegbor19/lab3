b5e212ebfee0443c75f88756211d438f0a5ac3c6
Class Account <ee106a-adf@c105-8.local>
2022-09-13 16:10:21 -0700

    initial

---
+++ build/106a-fa22-labs-starter/Lab3/forward_kinematics/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/cc/ee106a/fa22/class/ee106a-adf/ros_workspaces/lab3/src/106a-fa22-labs-starter/Lab3/forward_kinematics
+# Build directory: /home/cc/ee106a/fa22/class/ee106a-adf/ros_workspaces/lab3/build/106a-fa22-labs-starter/Lab3/forward_kinematics
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/cc/ee106a/fa22/class/ee106a-adf/ros_workspaces/lab3/src
+# Build directory: /home/cc/ee106a/fa22/class/ee106a-adf/ros_workspaces/lab3/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("106a-fa22-labs-starter/Lab3/forward_kinematics")
+subdirs("106a-fa22-labs-starter/Lab3/joint_ctrl")
